fc2cede741969a1905ddcb86811893913c519c20
ventshek <ventshek@gmail.com>
2022-11-04 11:29:47 +0100

    rg

---
+++ Go: The Complete Developer's Guide (Golang)/Sections/Section2/project/main.go
@@ -0,0 +1,12 @@
+/* Package == Project == Workspace */
+/* Main = The function named main is automaticall run when built. */
+package main
+
+/* Give my package main access to all the code in "fmt". This is a standard lib but Reusable Packages can be imported also. */
+import "fmt"
+
+/* func = function. main = the name of the function. () = the list of arguments passed to the function. {} = the function body. */
+
+func main() {
+	fmt.Println("Hi there!")
+}
---
+++ Go: The Complete Developer's Guide (Golang)/Sections/Section3/project/main.go
@@ -0,0 +1,42 @@
+// package main
+
+// import "fmt"
+
+// /* var = new variable. card = the name of the variable. string = only a string will ever be assigned to the variable. */
+// func main() {
+// 	// var card string = "Ace of Spades"
+// 	// := only used when defining a new variable
+// 	//card := "Ace of Spades"
+// 	//card := newCard()
+// 	//fmt.Println(card)
+// 	cards := []string{"Ace of Diamonds", newCard()}
+// 	cards = append(cards, "Six of Spades")
+// 	// i = index of this element in the array. card = Current card we're interating over. range cards = Take the slice of cards and loop over it. fmt.Println(card) Run this one time for each card in the slice.
+// 	for i, card := range cards {
+// 		fmt.Println(i, card)
+// 	}
+// 	//fmt.Println(cards)
+// }
+
+// // string = returns a value of type string when executing this function.
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
+
+package main
+
+//import "fmt"
+
+func main() {
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+	//cards := newDeckFromFile("my_cards")
+	//cards.print()
+	//cards := newDeck()
+	//cards.saveTofile("my_cards")
+	//fmt.Println(cards.toString())
+	//hand, remainingCards := deal(cards, 5)
+	//hand.print()
+	//remainingCards.print()
+}
