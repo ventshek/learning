3928c4010867503d173a2e0875d00a8f0a43bde1
ventshek <ventshek@gmail.com>
2022-11-06 10:12:00 +0100

    Update main.py

---
+++ The Complete Python Course/Section1/main.py
@@ -0,0 +1,54 @@
+# Define a variable
+# Vaiable names can have numbers and _s but no number at the beginning. 
+age = 30 
+print(age)
+# A variable that you intend to never change
+PI = 3.14159
+
+# // Gets rid of the float and makes it an int - does not round.
+maths_operation = 1 + 3 * 4 // 2 - 2
+print(maths_operation)
+
+integer_division = 13 // 5
+print(integer_division) 
+
+# Modulus operator(%), devides and then calculates what is left over
+remainder = 13 % 5
+print(remainder)
+
+x = 37 
+remainder = x % 2
+print(remainder)
+
+
+"""
+Convert int to a string.
+Or use printf to print the int withinn {}.
+"""
+
+age = 34
+age_as_str = str(age)
+print("You are " + age_as_str)
+
+print(f"You are {age}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
