7d09a5117007f8c68a53bd5569b6ae40442fe79a
ventshek <82648360+ventshek@users.noreply.github.com>
2022-11-21 15:37:07 +0100

    fghgfh

---
+++ Go: The Complete Developer's Guide/Sections/Section5/project/main.go
@@ -0,0 +1,30 @@
+package main
+
+
+import "fmt"
+
+
+func main() {
+	colors := map[string]string{
+		"red": "#ff0000",
+		"green": "#00ff00",
+	}
+	colors["white"] = "#ffffff"
+	//delete(colors, "white")
+	//fmt.Println(colors)
+	printMap(colors)
+
+
+
+
+
+
+
+}
+
+func printMap(c map[string]string) {
+	for key,value := range c {
+		fmt.Println("Hex code for", key, "is", value)
+
+	}
+}
