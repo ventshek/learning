165e1caa38a31c675637649a9bd6d9ef63eadd28
ventshek <82648360+ventshek@users.noreply.github.com>
2022-12-07 14:47:29 +0100

    gt

--- The Complete Python Course/Section1/main.py
+++ The Complete Python Course/Section1/main.py
@@ -1,5 +1,3 @@
-"""
-
 # Define a variable
 # Vaiable names can have numbers and _s but no number at the beginning. 
 age = 30 
@@ -23,18 +21,18 @@ remainder = x % 2
 print(remainder)
 
 
-"""
-"""
+
+
 Convert int to a string.
 Or use printf to print the int withinn {}.
-"""
-"""
+
+
 age = 34
 age_as_str = str(age)
 print("You are " + age_as_str)
 
 print(f"You are {age}")
-"""
+
 
 
 
